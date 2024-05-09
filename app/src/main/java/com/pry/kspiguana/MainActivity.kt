667dc289e5099d20d90ab8f7013aba430eda626e
juan-23contreras <159195813+juan-23contreras@users.noreply.github.com>
2024-05-09 12:49:20 -0600

    Curso
    pruebas

--- app/src/main/java/com/pry/kspiguana/MainActivity.kt
+++ app/src/main/java/com/pry/kspiguana/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.pry.kspiguana
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.toggleable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pry.kspiguana.ui.theme.KsPIguanaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyLazyColum()
                 }
             }
         }
@@ -30,17 +49,61 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyLazyColum() {
+    Column(
+        Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        HelloWorld("juan")
+        val (checkedState, onStateChange) = remember { mutableStateOf(true) }
+        MycheckBox(checkedState, onStateChange)
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun MycheckBox(checkedState: Boolean, onStateChange: (Boolean) -> Unit) {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .toggleable(
+                value = checkedState,
+                onValueChange = { onStateChange(!checkedState) },
+                role = Role.Checkbox
+            )
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checkedState,
+            onCheckedChange = null // null recommended for accessibility with screenreaders
+        )
+        Text(
+            text = "Option selection",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
+}
+
+@Composable
+fun HelloWorld(name: String) {
+    val context = LocalContext.current
+    Button(
+        onClick = {
+            Toast.makeText(context, name, Toast.LENGTH_LONG).show()
+        },
+        modifier = Modifier.padding(10.dp)
+    ) {
+        Text(text = stringResource(id = R.string.hello_worl)+"  $name", fontSize = 20.sp)
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
-    KsPIguanaTheme {
-        Greeting("Android")
-    }
+    //HelloWorld(name = "juan")
+    val (checkedState, onStateChange) = remember { mutableStateOf(true) }
+    MycheckBox(checkedState, onStateChange)
 }
